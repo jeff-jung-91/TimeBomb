822fc6ef2f050a8514f344d7b3f6fcff711d4270
jeff-jung-91 <38883364+Blackplace@users.noreply.github.com>
2020-04-10 11:31:06 +0900

    Initial Commit

---
+++ TimeBomb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeBomb
+//
+//  Created by jeff on 2020/04/10.
+//  Copyright © 2020 TakHyun Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
